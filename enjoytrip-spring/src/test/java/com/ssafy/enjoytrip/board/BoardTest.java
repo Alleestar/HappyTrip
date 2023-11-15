4d9a2803f4c5e947198efbe284ae97f82b73b0da
Master.Lee <77578710+alexander930828@users.noreply.github.com>
2023-11-15 21:36:27 +0900

    Update BoardTest #2

---
+++ enjoytrip-spring/src/test/java/com/ssafy/enjoytrip/board/BoardTest.java
@@ -0,0 +1,12 @@
+package com.ssafy.enjoytrip.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardTest {
+
+	@Test
+	void contextLoads() {
+	}
+}
